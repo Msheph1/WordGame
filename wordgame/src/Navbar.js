ede5366c871aee2980e5212994f2bc73016d28a1
Msheph1 <max.r.shep@gmail.com>
2024-08-21 18:52:23 -0500

    testing react

---
+++ wordgame/src/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
